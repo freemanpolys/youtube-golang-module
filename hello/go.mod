5ea88dbe221411c55b8e0f3e44111b8b129dd419
James Kokou GAGLO <freemanpolys@gmail.com>
2021-07-25 15:06:28 +0200

    Module

---
+++ hello/go.mod
@@ -0,0 +1,7 @@
+module hello
+
+replace world => ../world
+
+go 1.16
+
+require world v0.0.0-00010101000000-000000000000
---
+++ hello/main.go
@@ -0,0 +1,9 @@
+package main
+import(
+	"fmt"
+        "world"
+)
+
+func main() {
+ fmt.Println("Hello world:", world.Text)
+}
